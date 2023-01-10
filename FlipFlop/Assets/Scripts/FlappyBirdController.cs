92c95867945cf005e543fb23e8057dccf66ecc67
Matilda <97434498+matilda-t1@users.noreply.github.com>
2023-01-10 21:37:04 +0200

    scripts, sprites, prefabs

---
+++ FlipFlop/Assets/Scripts/FlappyBirdController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FlappyBirdController : MonoBehaviour {
+
+    public Quaternion forwardPosition;
+    public Quaternion fallingPosition;
+
+    public AudioSource source;
+
+    public static bool isGameStarted = false;
+
+    bool isPaused;
+
+    public bool IsDead { get; private set; }
+
+	public void Start ()
+    {
+        transform.localPosition = new Vector3(-0.93f, 0, 10);
+        source = GetComponent<AudioSource>();
+        IsDead = false;
+        isGameStarted = false;
+        forwardPosition = Quaternion.Euler(0, 0, 20);
+        fallingPosition = Quaternion.Euler(0, 0, -90);
+
+        StartCoroutine(AutoFlap());
+    }
+	
+	void Update ()
+    {
+        MoveCameraForward();
+
+        if (Input.GetMouseButtonDown(0) && isGameStarted && !isPaused && !IsDead)
+        {
+            source.Play();
+            Flap();
+        }
+        //Jos peli on käynnissä, käännä maata kohti
+        if(!isPaused)
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.rotation.eulerAngles), fallingPosition, 0.05f);
+	}
+
+    void Flap()
+    {
+        transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.rotation.eulerAngles), forwardPosition, 7f);
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * 150, ForceMode2D.Force);
+    }
+
+    void MoveCameraForward()
+    {
+        if(!isPaused && !IsDead)
+            transform.parent.Translate(new Vector3(0.01f, 0));
+    }
+
+    IEnumerator AutoFlap()
+    {
+        while (!isGameStarted)
+        {
+            if (transform.position.y <= 0)
+                Flap();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    //jos pysäytetty, ei putoa(painovoima lakkaa toimimasta). Jos peli on käynnissä, tekee toisinpäin
+    public void Pause(bool isPaused)
+    {
+        this.isPaused = isPaused;
+        if (isPaused)
+        {
+            GetComponent<Rigidbody2D>().simulated = false;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().simulated = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Pipe" || collider.gameObject.tag == "Ground")
+            Die();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Game_Manager.instance.AddScorePoint();
+    }
+
+    void Die()
+    {
+        if (!IsDead && isGameStarted)
+        {
+            IsDead = true;
+            Game_Manager.instance.Die();
+        }
+    }
+}
